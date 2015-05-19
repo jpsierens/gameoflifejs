5b2c0c73a38e6f0ffe433c72896eebbd70f724a7
jpsierens <jpsierens1988@gmail.com>
2015-05-19 16:24:38 -0600

    grid start

--- app.js
+++ app.js
@@ -1,7 +1,32 @@
 import Universe from './js/Universe';
 
+//const cus I don't want them to change.
+// in cell units
+const UNIVERSE_LENGTH = 15;
+const UNIVERSE_HEIGHT = 15;
+// in pixels
+const CELL_LENGTH = 32;
+const CELL_HEIGHT = 32;
+
 var universe = new Universe();
-universe.create();
+universe.create(UNIVERSE_LENGTH, UNIVERSE_HEIGHT);
+
+
+// drawing the universe
+var canvas = document.getElementById('universe');
+var ctx = canvas.getContext('2d');
+drawGrid();
+
+// draw grid
+function drawGrid() {
+	// vertical lines
+	for (let i = 1; i<UNIVERSE_LENGTH; i++){
+		ctx.beginPath();
+		ctx.moveTo(CELL_LENGTH*i,0);
+		ctx.lineTo(CELL_LENGTH*i, UNIVERSE_HEIGHT*CELL_HEIGHT);
+		ctx.stroke();
+	}
+}
 
 
 
--- js/Universe.js
+++ js/Universe.js
@@ -1,19 +1,15 @@
-//const cus I don't want them to change.
-const UNIVERSE_LENGTH = 15;
-const UNIVERSE_HEIGHT = 15;
-const CELL_LENGTH = 32;
-const CELL_HEIGHT = 32;
-
 class Universe {
-	constructor(){
+	constructor(length, height){
 		// this will be the tilemap, 2D matrix representing the universe
 		this.cells = [];
+		this.height = height;
+		this.length = length;
 	}
 	create() {
 		// Assign the tilemap in relation with the length and height of the universe.
-		for (let i = 0; i<UNIVERSE_HEIGHT; i++) {
+		for (let i = 0; i<this.height; i++) {
 			this.cells.push(new Array());
-			for (let j = 0; j<UNIVERSE_LENGTH; j++) {
+			for (let j = 0; j<this.length; j++) {
 				// at the initial universe, every cell is dead (0)
 				this.cells[i].push(0)
 			}
