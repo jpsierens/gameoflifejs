05cf933d2a94a95087bc50c7d6d386ae8c6dda5a
isrd1 <rob.davis@x-alt.com>
2018-04-07 23:21:27 +0100

    sourcemaps added to build

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, '.')
   },
-
+  devtool: 'source-map',
   module: {
     rules: [{
       test: /\.js$/, // Run the loader on all .js files
